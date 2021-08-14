49839deaa3bc8a86e1e5f5f18a9c70767a9fed42
Nhat Tong <thqnhat@gmail.com>
2021-08-14 15:23:56 +0200

    update python script

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -3,21 +3,21 @@ import random
 import sys
 
 # Make an HTTP GET request to the cat-fact API
-cat_url = "https://cat-fact.herokuapp.com/facts"
-r = requests.get(cat_url)
-r_obj_list = r.json()
+# cat_url = "https://cat-fact.herokuapp.com/facts"
+# r = requests.get(cat_url)
+# r_obj_list = r.json()
 
-fact_list = []
+# fact_list = []
 
-for fact in r_obj_list:
-    fact_list.append(fact["text"])
+# for fact in r_obj_list:
+#     fact_list.append(fact["text"])
 
-# Select a random fact from the fact_list and return it
-# into a variable named random_fact so we can use it
-def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)+1)]
+# # Select a random fact from the fact_list and return it
+# # into a variable named random_fact so we can use it
+# def select_random_fact(fact_arr):
+#     return fact_arr[random.randint(0, len(fact_list)+1)]
 
-random_fact = select_random_fact(fact_list)
+random_fact = "The frequency of a domestic cat;s purr is the same at which muscles and bones repair themselves."
 
 # Print the individual randomly returned cat-fact
 print(random_fact)
